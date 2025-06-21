c2014b406fd67873416e07a4852101f16b53bba5
Vitalii <saifutdinov_vitalii@mail.ru>
2025-06-21 15:29:10 +0500

    add to page

--- js/script.js
+++ js/script.js
@@ -16,4 +16,40 @@ document.addEventListener('DOMContentLoaded', ()=> {
         })
 
     //Открытие каждого элемента меню
+    // Галлерея
+    const galerey = document.querySelector('.galerey');
+    if(galerey) {
+        baguetteBox.run('.galerey-buget');
+    }
+    // Галлерея
+    // Модальное окно
+    const modal = document.querySelector('.modal');
+    const modalBtn = document.querySelector('[data-modal]');
+
+    
+
+    function removeModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+        document.body.style.overflow = '';
+    };
+
+    function showModal() {
+        modal.classList.remove('hide');
+        modal.classList.add('show');
+        document.body.style.overflow = 'hidden';
+    };
+
+    modalBtn.addEventListener('click', ()=> showModal());
+    modal.addEventListener('click', (e)=> {
+        if(e.target === modal || e.target.classList.contains('modal-close')) {
+            removeModal();
+        }
+    });
+    window.addEventListener('keydown', (e)=> {
+        if(e.code === 'Escape' || modal.classList.contains('show')) {
+            removeModal();
+        }
+    })
+    // Модальное окно
 })
